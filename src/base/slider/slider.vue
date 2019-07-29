314a16d5d268113ce07921307d0f0d05a3adbd9a
houxy <1531604938@qq.com>
2019-07-29 19:44:39 +0800

    slider开发完毕

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -5,7 +5,10 @@
       <slot></slot>
     </div>
     <div class="dots">
-
+      <span class="dot" v-for="(item,index) in dots" :keys="index"
+      :class="{active: currentPageIndex === index}"
+      >
+      </span>
     </div>
   </div>
 </template>
@@ -30,20 +33,36 @@
       }
     },
     data() {
-      return {}
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
     },
     mounted() {
       this.$nextTick(() => {
         this._setSliderWidth();
+        this._initDots();
         this._initSlider();
+
+        if(this.autoplay){
+          this._play();
+        }
       })
       /*setTimeout(() => {
         this._setSliderWidth();
         this._initSlider();
       },20)*/
+      //监听屏幕的尺寸，从新获取width
+      window.addEventListener('resize', () => {
+        if(!this.slider){
+          return
+        }
+        this._setSliderWidth(true);
+        this.slider.refresh(); //重新刷新better-scroll
+      })
     },
     methods: {
-      _setSliderWidth() {
+      _setSliderWidth(isResize) {
         this.children = this.$refs.sliderGroup.children;
 
         let width = 0;
@@ -56,7 +75,7 @@
           width += sliderWidth;
         }
 
-        if(this.loop){
+        if(this.loop && !isResize){
           width += 2 * sliderWidth
         }
         this.$refs.sliderGroup.style.width = width + 'px'
@@ -70,8 +89,33 @@
           snapLoop: this.loop,
           snapThreshold: 0.3,
           snapSpeed: 400,
-          click: true
+          //click: true
         });
+
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX;
+          if(this.loop){
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex;
+
+          if(this.autoPlay){
+            clearTimeout(this.timer);
+            this._play();
+          }
+        })
+      },
+      _initDots() {//初始化dot点
+        this.dots = new Array(this.children.length);
+      },
+      _play() {//自动播放的方法
+        let pageIndex = this.currentPageIndex + 1;
+        if(this.loop){
+          pageIndex += 1;
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400)
+        },this.interval);
       }
     }
   }
