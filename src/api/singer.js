cc2ba9a76a1dd59739ed9edd570ca893088aa8e6
houxy <1531604938@qq.com>
2019-08-01 15:24:35 +0800

    '抓取歌手详情页的数据，处理歌单详情页刷页面的问题'

--- src/api/singer.js
+++ src/api/singer.js
@@ -3,6 +3,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
+//歌手列表数据
 export function getSingerList() {
   const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
 
@@ -16,6 +17,25 @@ export function getSingerList() {
     needNewCode: 0,
     platform: 'yqq',
     g_tk: 1664029744
+  });
+
+  return jsonp(url, data, options)
+}
+
+//歌手详情页面数据
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 80,
+    songstatus: 1,
+    g_tk: 1664029744,
+    singermid: singerId
   });
 
   return jsonp(url, data, options)
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -5,6 +5,8 @@
 
 <script>
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from "../../api/singer";
+  import {ERR_OK} from "../../api/config";
 
   export default {
     data() {
@@ -16,6 +18,20 @@
       ])
     },
     created() {
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if(!this.singer.id){
+          this.$router.push('/singer');
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if(res.code === ERR_OK){
+            console.log(res.data.list)
+          }
+        })
+      }
     }
   }
 </script>
