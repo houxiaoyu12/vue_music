c5aef499c8e47b9e70bcbe7cf98a60059aa99e9b
houxy <1531604938@qq.com>
2019-11-18 17:26:41 +0800

    'CD放大缩小动画'

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -3,13 +3,16 @@
   <div class="player" v-show="playList.length > 0">
     <!--展开的播放器-->
     <transition name="normal"
-
-      >
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
       <div class="normal-player" v-show="fullScreen">
           <!--<div class="background">
               <img width="100%" height="100%" :src="currentSong.imag">
           </div>-->
-          <div class="top" @click="back">
+          <div class="top">
               <div class="back" @click="back">
                   <i class="icon-back"></i>
               </div>
@@ -97,6 +100,10 @@
 
 <script>
   import {mapGetters, mapMutations,} from 'vuex'
+  import animations from 'create-keyframe-animation' //js动画github API组件
+  import {prefixStyle} from 'common/js/dom'
+
+  const transform = prefixStyle('transform')
 
   export default {
     data() {
@@ -116,6 +123,58 @@
       },
       open() {
         this.setFullScreen(true)
+      },
+      enter(el, done) {
+        const {x, y, scale} = this._getPosAndScale();
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        };
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 600, //间隔时间
+            easing: 'linear' //缓动曲线
+          }
+        });
+         animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter() {
+        animations.unregisterAnimation('move');
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      leave(el, done) {
+        this.$refs.cdWrapper.style.transition = 'all 0.8s'
+        const {x, y, scale} = this._getPosAndScale()
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend', done)
+      },
+      afterLeave() {
+        this.$refs.cdWrapper.style.transition = ''
+        this.$refs.cdWrapper.style[transform] = ''
+      },
+      _getPosAndScale() {
+        const targetWidth = 40;
+        const paddingLeft = 40;
+        const paddingBottom = 30;
+        const paddingTop = 80;
+        const width = window.innerWidth * 0.8;
+        const scale = targetWidth/ width;
+        const x = -(window.innerWidth/2 - paddingLeft); //x轴位移的距离
+        const y = window.innerHeight - paddingTop - width/2 - paddingTop;
+        return {
+          x,
+          y,
+          scale
+        }
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN'
