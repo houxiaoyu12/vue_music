9f98f21907dfe6f4d8902cce50bdcfb7bf3adab6
houxy <1531604938@qq.com>
2020-01-06 16:43:23 +0800

    '搜索框待完善'

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input ref="query" v-model="query" class="box" :placeholder="placeholder"/>
+    <i v-show="query" class="icon-dismiss" @click="clear"></i>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  //import {debounce} from 'common/js/util'
+
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: '搜索歌曲、歌手'
+      }
+    },
+    data () {
+      return {
+        query: ''
+      }
+    },
+    methods: {
+      clear () {
+        this.query = ''
+      },
+      setQuery(query) {
+        this.query = query
+      }
+    },
+    created () {
+      this.$watch('query', (newQuery) => {
+        this.$emit('query', newQuery)
+      })
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "./index.styl"
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>search</div>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li @click="addQuery(item.k)" class="item" v-for="(item, index) in hotKey" :key="index">
+              <span>{{item.k}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {}
-        }
+  import SearchBox from '../../base/search-box/search-box'
+  import { getHotKey } from '../../api/search'
+  import { ERR_OK } from '../../api/config'
+
+  export default {
+    data () {
+      return {
+        hotKey: []
+      }
+    },
+    created () {
+      this._getHotKey()
+    },
+    methods: {
+      addQuery(query) {
+        this.$refs.searchBox.setQuery(query)
+      },
+      _getHotKey () {
+        getHotKey().then((res) => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      }
+    },
+    components: {
+      SearchBox,
     }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  @import "./index.styl"
 </style>
